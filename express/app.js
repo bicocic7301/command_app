309ae708b2a09a7aea18af0197441e4f90a791b7
bicocic7301 <bicocic7301@outlook.com>
2025-11-18 20:50:25 +0000

    added shell

--- express/app.js
+++ express/app.js
@@ -2,11 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require("cors");
+
 app.use(cors());  // Enable CORS
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' }); 
-})
+  var spawn = require("child_process").spawn,child;
+  output = "";
+  child = spawn("powershell.exe",["ls"]);
+  child.stdout.on("data",function(data){
+      output += `${data}`;
+
+  });
+  child.stderr.on("data",function(data){
+      console.log("Powershell Errors: " + data);
+  });
+  child.on("exit",function(){
+      console.log("Powershell Script finished");
+      res.json({ message: output }); 
+  });
+  child.stdin.end(); //end input
+  
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
